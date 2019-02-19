fe80f6c943bae38d35cb9a06234d27c433702671
daryaav <36270322+daryaav@users.noreply.github.com>
2019-02-19 18:50:01 +0100

    Começam projecte...

---
+++ app/src/main/java/volkodav/ampilogova/darya/projecte_missatgeria/MainActivity.java
@@ -0,0 +1,13 @@
+package volkodav.ampilogova.darya.projecte_missatgeria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
